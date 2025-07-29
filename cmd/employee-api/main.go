444b5c2b7c0397f92537e81345a081d787164599
udaymungalpara <udayhn2005@gmail.com>
2025-07-29 18:22:20 +0530

    server shutdown

--- cmd/employee-api/main.go
+++ cmd/employee-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/udaymungalpara/employee-api/internal/config"
 )
@@ -27,10 +33,29 @@ func main() {
 		Addr:    cfg.Add,
 		Handler: router,
 	}
-	err := server.ListenAndServe()
-	fmt.Println("strated server")
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		fmt.Println("strated server")
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server")
+		}
+	}()
+
+	<-done
+	slog.Info("shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
 	if err != nil {
-		log.Fatal("Failed to start server")
+		slog.Info("server not shutdown")
 	}
 
+	slog.Info("server shutdown")
 }
